67f66f256c6e44072307c5e593696c04d80d1a06
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-22 12:46:35 -0400

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainLayout from '../src/components/layout/MainLayout/MainLayout';
+import Dashboard from './components/views/Dashboard/Dashboard';
+import Login from '../src/components/views/Login/Login';
+import Tables from '../src/components/views/Tables/Tables';
+import Waiter from '../src/components/views/Waiter/Waiter';
+import Kitchen from '../src/components/views/Kitchen/Kitchen';
 
 function App() {
   return (
-    <MainLayout>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React Right Now
-          </a>
-        </header>
-      </div>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
+          <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+          <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+          <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
+          <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
+
+          <div className="App">
+            <header className="App-header">
+              <p>
+          Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+          Learn React
+              </a>
+            </header>
+          </div>
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './Tables.modules.scss';
+import styles from './Tables.module.scss';
 
 const Tables = () => (
   <div className={styles.component}>

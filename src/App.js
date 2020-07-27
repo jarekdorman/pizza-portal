d40f36b3a38cac4d4d2763da334479a0036c3939
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-27 12:09:50 -0400

    Add material UI

--- src/App.js
+++ src/App.js
@@ -6,21 +6,54 @@ import Login from '../src/components/views/Login/Login';
 import Tables from '../src/components/views/Tables/Tables';
 import Waiter from '../src/components/views/Waiter/Waiter';
 import Kitchen from '../src/components/views/Kitchen/Kitchen';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#64b5f6',
+    },
+    // secondary: {
+    //   main: "#11cb5f",
+    // },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
-          <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-          <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-          <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
-          <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route
+                exact
+                path={`${process.env.PUBLIC_URL}/`}
+                component={Dashboard}
+              />
+              <Route
+                path={`${process.env.PUBLIC_URL}/login`}
+                component={Login}
+              />
+              <Route
+                path={`${process.env.PUBLIC_URL}/tables`}
+                component={Tables}
+              />
+              <Route
+                path={`${process.env.PUBLIC_URL}/waiter`}
+                component={Waiter}
+              />
+              <Route
+                path={`${process.env.PUBLIC_URL}/kitchen`}
+                component={Kitchen}
+              />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
-
   );
 }
 
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-// import styles from './PageNav.module .scss';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import styles from './PageNav.module.scss';
 
-const PageNav = () => (
-  <nav>
-    <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</NavLink>
-    <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</NavLink>
-    <NavLink to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</NavLink>
-    <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</NavLink>
-    <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
-  </nav>
-);
+const PageNav = () => {
+  return (
+    <nav className={styles.component}>
+      <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Home</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
+    </nav>
+  );
+};
 
 
 export default PageNav;
+
